430d1dcbb99cc8f002d5f90ec792db4de23ed942
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-01-19 10:45:51 +0200

    Rename functions inside decorator

--- vt/helper.py
+++ vt/helper.py
@@ -1,7 +1,7 @@
 from flask import Response
 
-def return_text(view):
-    def wrapped_view(*args, **kwargs):
-        return Response(view(*args, **kwargs), mimetype="text/plain;charset=UTF-8")
+def return_text(func):
+    def wrapped_response(*args, **kwargs):
+        return Response(func(*args, **kwargs), mimetype="text/plain;charset=UTF-8")
 
-    return wrapped_view
+    return wrapped_response
