cf8fa455d692b49e8bf8e9c36d8ebe3c3b7b1e5c
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-01-24 22:31:39 +0200

    Add docstrings and comments

--- vt/data/data.py
+++ vt/data/data.py
@@ -2,6 +2,7 @@
 import json
 import os
 import urllib.request
+from urllib.error import URLError
 
 DATA_REMOTE_URL = "http://hazor.eu.pythonanywhere.com/2021/data.json"
 DATA_FILENAME = "data.json"
@@ -17,15 +18,21 @@ def load_data(remote=False) -> dict:
     return data
 
 def save_data(data):
+    """Tallentaa datan tiedostoon"""
     with open(DATA_FILENAME, encoding="UTF-8", mode="w") as file:
         json.dump(data, file, ensure_ascii=False)
 
 
 def _reset_data():
     """Lataa oletusdatan palvelimelta ja korvaa sillä paikallisen datan"""
-    # TODO virheenkäsittely jos ei vastausta
-    with urllib.request.urlopen(DATA_REMOTE_URL) as response:
-        data = json.load(response)
-        save_data(data)
+    
+    data = None
+    try:
+        with urllib.request.urlopen(DATA_REMOTE_URL) as response:
+            data = json.load(response)
+            save_data(data)
+    except URLError:
+        # Ladataan paikallinen data jos palvelin ei vastaa
+        data = load_data(False)
 
     return data
--- vt/data/teams.py
+++ vt/data/teams.py
@@ -29,7 +29,7 @@ def parse_teams(data: dict):
 def names_to_string(teams: list):
     """Muodostaa joukkueiden nimistä merkkijonon, jossa joukkueet
     ovat aakkosjärjestyksessä rivinvaihdoilla erotettuna"""
-    return "\n".join(sorted(map(lambda team: team['nimi'], teams), key=lambda name: name.lower()))
+    return "\n".join(sorted((team['nimi'] for team in teams), key=lambda name: name.lower()))
 
 
 def add_team(series: dict, team: dict):
@@ -38,10 +38,12 @@ def add_team(series: dict, team: dict):
         team['id'] = generate_random_id(parse_teams(load_data(remote=False)))
     series['joukkueet'].append(team)
 
+
 def remove_team(series: dict, team_name: str):
-    print(team_name)
+    """Poistaa joukkueen sarjasta"""
     teams = [team for team in series['joukkueet'] if team['nimi'] != team_name]
     series['joukkueet'] = teams
+
 
 def generate_random_id(teams: list, iter: int=0):
     """Luo joukkueelle uniikin id:n"""
@@ -52,20 +54,16 @@ def generate_random_id(teams: list, iter: int=0):
 
     # arvotaan satunnainen id, yhteentörmäyksen sattuessa arvotaan uusi
     rdm = random.randint(1000000000000000,9999999999999999)
-    if rdm in map(lambda team: team['id'], teams):
+    if rdm in (team['id'] for team in teams):
         rdm = generate_random_id(teams, iter + 1)
 
     return rdm
 
 def calculate_points(team: dict, checkpoints: list):
-    #TODO reduce
+    """Laskee pisteet annetun joukkueen rastileimauksista"""
 
-    print("parsing punches for: " + team['nimi'])
+    # seulotaan rastileimauksista vain kelvolliset leimaukset
     parsed_punches = parse_punches(team['rastit'], checkpoints)
-
-    print()
-    print(f"{team['nimi']}: rastit:")
-    print(parsed_punches)
 
     total = 0
     for punch in parsed_punches:
@@ -75,25 +73,81 @@ def calculate_points(team: dict, checkpoints: list):
 
 
 def parse_punches(punches: list, checkpoints: list):
-    # TODO generator expression
+    """Seuloo rastileimauksista vain kelvolliset leimaukset. Leimatun rastin id:n
+    on vastattava todellista rastia ja leimauksen ajankohdan tulee olla lähtö- ja maalileimauksen
+    välissä.
+    
+    Palauttaa tuplen muodossa: (aika, koodi, lat, lon)
+    """
+
     punched_checkpoints = set()
     parsed_punches = []
     valid_punches = []
 
+    # Karsitaan ensimmäisellä iteraatiolla pois leimaukset jotka eivät vastaa todellista rastia
     for punch in punches:
         checkpoint = find_checkpoint_by_id(punch['rasti'], checkpoints)
         if checkpoint:
             parsed_punches.append((punch['aika'], checkpoint['koodi'], checkpoint['lat'], checkpoint['lon']))
 
-    start_end_timestamps = _get_start_end_punches(parsed_punches)
+    # Selvitetään lähtö- ja maalileimauksen ajankohdat
+    start_end_punches = _get_start_end_punches(parsed_punches)
 
+    # Karsitaan toisella iteraatiolla leimaukset, jotka ovat sallitun aikavälin ulkopuolella
+    # tai leimattu useammin kuin kerran
     for punch in parsed_punches:
-        if _is_valid_punch(punch, start_end_timestamps, punched_checkpoints):
+        if _is_valid_punch(punch, start_end_punches, punched_checkpoints):
             valid_punches.append(punch)
 
     return valid_punches
 
+
+def _get_start_end_punches(parsed_punches: list):
+    """Etsii myöhäisimmän lähtöleimauksen ja ensimmäisen maalileimauksen.
+    
+    Palauttaa dictionaryn muodossa {start: (lähtöleimaus, ajankohta), end: (maalileimaus, ajankohta)}"""
+    start_datetime = None
+    start_punch = None
+    end_datetime = None
+    end_punch = None
+
+    # Etsitään ensimmäisellä iteraatiolla myöhäisin lähtöleimaus
+    for punch in parsed_punches:
+        if punch[1] == 'LAHTO':
+            start_timestamp = punch[0]
+            temp_datetime = datetime.fromisoformat(start_timestamp) if start_timestamp else None
+
+            # Hyväksytään ensimmäinen vastaantuleva lähtöleimaus sekä leimaus jonka
+            # ajankohta on myöhemmin kuin aiemmin löydetty lähtöleimaus
+            if (not start_datetime or temp_datetime > start_datetime):
+                start_datetime = temp_datetime
+                start_punch = punch
+
+    # Etsitään toisella iteraatiolla ensimmäinen maalileimaus (vain jos lähtöleimaus löytyi)
+    if start_datetime:
+        for punch in parsed_punches:
+            if punch[1] == 'MAALI':
+                end_timestamp = punch[0]
+                temp_datetime = datetime.fromisoformat(end_timestamp) if end_timestamp else None
+
+                # Hyväksytään ensimmäinen vastaantuleva maalileimaus sekä leimaus jonka
+                # leimauksen ajankohta on myöhemmin kuin aiemmin löydetty lähtöleimaus.
+                # Leimauksen tulee olla lähtöleimauksen jälkeen.
+                if (temp_datetime > start_datetime and (not end_datetime or temp_datetime < end_datetime)):
+                    end_datetime = temp_datetime
+                    end_punch = punch
+
+    return {
+        'start': (start_punch, start_datetime),
+        'end': (end_punch, end_datetime)
+        }
+
+
 def _is_valid_punch(punch, start_end_punches: dict, punched_checkpoints: set):
+    """Tarkistaa onko rastileimauksen aikaleima lähtö- ja maalileimauksen välissä ja
+    onko rasti leimattu vain yhden kerran"""
+
+    # Jos rasti on jo leimattu, ei kelpuuteta toista leimausta
     if punch[1] in punched_checkpoints:
         return False
 
@@ -101,40 +155,14 @@ def _is_valid_punch(punch, start_end_punches: dict, punched_checkpoints: set):
     start = start_end_punches['start'][1]
     end = start_end_punches['end'][1]
 
-    print(f"{timestamp}, start={start}, end={end}")
+    # Kelpuutetaan leimaus vain jos se on sallitulla aikavälillä
     valid = start < timestamp < end
     if valid:
+        # Lisätään kelvollisten leimausten joukkoon
         punched_checkpoints.add(punch[1])
 
     return valid
 
-
-def _get_start_end_punches(parsed_punches: list):
-    start_datetime = None
-    start_punch = None
-    end_datetime = None
-    end_punch = None
-
-    for punch in parsed_punches:
-        if punch[1] == 'LAHTO':
-            start_timestamp = punch[0]
-            temp_datetime = datetime.fromisoformat(start_timestamp) if start_timestamp else None
-            if (not start_datetime or temp_datetime > start_datetime):
-                start_datetime = temp_datetime
-                start_punch = punch
-
-    if start_datetime:
-        for punch in parsed_punches:
-            if punch[1] == 'MAALI':
-                end_timestamp = punch[0]
-                temp_datetime = datetime.fromisoformat(end_timestamp) if end_timestamp else None
-                if (temp_datetime > start_datetime and (not end_datetime or temp_datetime < end_datetime)):
-                    end_datetime = temp_datetime
-                    end_punch = punch
-
-    return {
-        'start': (start_punch, start_datetime),
-        'end': (end_punch, end_datetime)}
 
 def print_teams(teams: list):
     """Muodostaa joukkueiden nimistä merkkijonon, jossa joukkueet
