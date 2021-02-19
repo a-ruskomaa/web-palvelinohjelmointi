83a37b060ebb9cee7e426d2984f111f8ac4f2751
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-02-19 13:55:29 +0200

    Added login endpoint + form

--- vt3/tupa/__init__.py
+++ vt3/tupa/__init__.py
@@ -1,30 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 
 print("initializing app...")
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'supersecret'
     
     print("creating app...")
 
-    from .views import admin
+    from .views import admin, login
     app.register_blueprint(admin.bp)
+    app.register_blueprint(login.bp)
 
-    from tupa.modules.data import init_db, get_connection, close_connection
-
+    from tupa.modules.data import init_db
+    
     init_db(app)
 
-    @app.route('/', methods=['GET'], strict_slashes=False)
+    @app.route('/', methods=["GET"])
     def index():
-        con = get_connection()
-        cur = con.cursor()
-
-        sql = """
-        SELECT nimi
-        FROM kilpailut
-        """
-        cur.execute(sql)
-        kilpailut = cur.fetchall()
-        return render_template('index.html', kilpailut=kilpailut)
+        print("ETUSIVU")
+        if session.get('joukkue'):
+            return redirect(url_for('joukkuesivu'))
+        else:
+            return redirect(url_for('login.login'))
             
     return app
